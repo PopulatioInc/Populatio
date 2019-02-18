960b9a9186be122f7e35bb9a5939063961f61ddf
domm98 <dominicmanderson11@gmail.com>
2019-02-18 14:36:50 +0000

    Comments

--- src/main/java/com/napier/sem/entities/City.java
+++ src/main/java/com/napier/sem/entities/City.java
@@ -9,21 +9,21 @@ public class City {
     public City getCity(int ID, Connection con) {
         try {
             // Create an SQL statement
-            Statement stmt = con.createStatement();
+            Statement stmt = con.createStatement(); //Create mySQL statement placeholder
             // Create string for SQL statement
             String strSelect =
-                    "SELECT ID, Name, CountryCode, District, Population "
+                    "SELECT ID, Name, CountryCode, District, Population " //Statement takes in SQL request
                             + "FROM city "
-                            + "WHERE ID = " + ID;
+                            + "WHERE ID = " + ID; //currently based on ID provided by getCity
             // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
+            ResultSet rset = stmt.executeQuery(strSelect); //Executes the SQL query and saves it to rset
             // Return new city if valid.
             // Check one is returned
             if (rset.next()) {
-                City c = new City();
-                c.id = rset.getInt("ID");
-                c.name = rset.getString("Name");
-                c.countrycode = rset.getString("CountryCode");
+                City c = new City(); //Create placeholder for City
+                c.id = rset.getInt("ID"); //Get ID and put into City
+                c.name = rset.getString("Name"); //Get Name and put into City
+                c.countrycode = rset.getString("CountryCode"); //and so on.
                 c.district = rset.getString("District");
                 c.population = rset.getInt("Population");
                 return c;
@@ -36,7 +36,7 @@ public class City {
         }
     }
 
-    public void displayCity(City c)
+    public void displayCity(City c) //Displays the city data.
     {
         if (c != null)
         {
