7e6cb0fa1de42044104784dae6863d9fb6805ec9
Odysseius <galaxy2alex@gmail.com>
2019-02-18 11:03:17 +0000

    Change to world db and add basic City and Country classes

---
+++ src/main/java/com/napier/sem/entities/City.java
@@ -0,0 +1,12 @@
+package com.napier.sem.entities;
+
+public class City {
+
+    public String name;
+
+    public String countrycode;
+
+    public String district;
+
+    public int population;
+}
---
+++ src/main/java/com/napier/sem/entities/Country.java
@@ -0,0 +1,17 @@
+package com.napier.sem.entities;
+
+public class Country {
+
+    public String countrycode;
+
+    public String name;
+
+    public String continent;
+
+    public String region;
+
+    public int population;
+
+    public String capital;
+
+}
